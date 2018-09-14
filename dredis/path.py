a4a958921bb844ee0a2dc4dc2e4ed117892c58f0
Hugo Lopes Tavares <hltbra@gmail.com>
2018-09-14 17:24:12 -0400

    Add type annotation to path.py
    Used by mypy

--- dredis/path.py
+++ dredis/path.py
@@ -7,47 +7,59 @@ import six
 import sys
 import tempfile
 
+from typing import Any, List
+
 
 class Path(str):
 
     def __init__(self, path):
+        # type: (Any[str, Path]) -> None
         self._path = path
 
     def join(self, path):
+        # type: (Any[str, Path]) -> Path
         return Path(os.path.join(self._path, path))
 
     def reset(self):
+        # type: () -> None
         shutil.rmtree(self._path, ignore_errors=True)
         os.makedirs(self._path)
 
     def read(self):
+        # type: () -> Any
         with open(self._path, 'r') as f:
             result = f.read()
         return self._deserialize(result)
 
     def write(self, content):
+        # type: (str) -> None
         with open(self._path, 'w') as f:
             f.write(self._serialize(content))
 
     def delete(self):
+        # type: () -> None
         if os.path.isfile(self._path):
             os.remove(self._path)
         else:
             shutil.rmtree(self._path)
 
     def append(self, line):
+        # type: (str) -> None
         with open(self._path, 'a') as f:
             f.write(self._serialize(line) + '\n')
 
     def readlines(self):
+        # type: () -> List[str]
         with open(self._path) as f:
             lines = f.readlines()
-        return map(self._deserialize, lines)
+        return list(map(self._deserialize, lines))
 
     def exists(self):
+        # type: () -> bool
         return os.path.exists(self._path)
 
     def makedirs(self, ignore_if_exists=False):
+        # type: (bool) -> None
         try:
             return os.makedirs(self._path)
         except OSError as exc:
@@ -57,6 +69,7 @@ class Path(str):
                 six.reraise(*sys.exc_info())
 
     def listdir(self, pattern=None):
+        # type: (Any[str, None]) -> List[str]
         all_files = os.listdir(self._path)
         if pattern is None:
             return all_files
@@ -64,6 +77,7 @@ class Path(str):
             return list(fnmatch.filter(all_files, pattern))
 
     def remove_line(self, line_to_remove):
+        # type: (str) -> None
         with tempfile.NamedTemporaryFile('w', delete=False) as tfile:
             for line in self.readlines():
                 if line != line_to_remove:
@@ -72,10 +86,13 @@ class Path(str):
         os.rename(tfile.name, self._path)
 
     def empty_directory(self):
+        # type: () -> bool
         return self.exists() and not self.listdir()
 
     def _deserialize(self, value):
+        # type: (str) -> Any
         return json.loads(value)
 
     def _serialize(self, value):
+        # type: (str) -> str
         return json.dumps(value)
