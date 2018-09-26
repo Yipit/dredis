Yipit/dredis

commit 3e80c6d9768ba66f178dded1ee5b47b4cdce86b2

Improve performance of zset when rescoring elements
Truncating file in place instead of os.rename

dredis/path.py
@@ -5,7 +5,6 @@ import os.path
 import shutil
 import six
 import sys
-import tempfile
 
 
 class Path(str):
@@ -61,12 +60,11 @@ class Path(str):
             return list(fnmatch.filter(all_files, pattern))
 
     def remove_line(self, line_to_remove):
-        with tempfile.NamedTemporaryFile('w', delete=False) as tfile:
-            for line in self.readlines():
+        lines = self.readlines()
+        with open(self, 'w') as f:
+            for line in lines:
                 if line != line_to_remove:
-                    tfile.write(self._serialize(line) + "\n")
-            tfile.close()
-        os.rename(tfile.name, self)
+                    f.write(line)
 
     def empty_directory(self):
         return self.exists() and not self.listdir()

tests-performance/test_zset_performance.py
@@ -16,6 +16,15 @@ def test_zadd():
     print '\nZADD time = {}s'.format(after_zadd - before_zadd)
 
 
+def test_zadd_rescore_same_element():
+    r = fresh_redis(port=PROFILE_PORT)
+    before_zadd = time.time()
+    for score in range(LARGE_NUMBER):
+        assert r.zadd('myzset', score, 'value') == 1
+    after_zadd = time.time()
+    print '\nZADD time = {}s'.format(after_zadd - before_zadd)
+
+
 def test_zcard():
     r = fresh_redis(port=PROFILE_PORT)
     for score in range(LARGE_NUMBER):

commit 129867ec4b3f3ed0842d69fdf1a622b93a4455c2

Fix serialization bug with remove_line
This bug was introduced by commit 3e80c6d9768ba66f178dded1ee5b47b4cdce86b2

dredis/path.py
@@ -64,7 +64,7 @@ class Path(str):
         with open(self, 'w') as f:
             for line in lines:
                 if line != line_to_remove:
-                    f.write(line)
+                    f.write(self._serialize(line) + "\n")
 
     def empty_directory(self):
         return self.exists() and not self.listdir()