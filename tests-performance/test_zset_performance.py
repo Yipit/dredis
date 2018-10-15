a52063fa49cfd72694087b1a86a6de624b4521aa
Hugo Lopes Tavares <hltbra@gmail.com>
2018-10-15 18:18:55 -0400

    Update zset performance test results

--- tests-performance/test_zset_performance.py
+++ tests-performance/test_zset_performance.py
@@ -2,16 +2,16 @@
 The following results should serve as reference
 ------
 
-Results from 2018-10-12 on @htlbra's Macbook (LARGE_NUMBER == 1000):
+Results from 2018-10-15 on @htlbra's Macbook (LARGE_NUMBER == 1000):
 
-$ make performance-server & make test-performance | grep time
-ZADD time = 0.45322s
-ZADD time = 0.66988s
-ZCARD time = 0.00116s
-ZRANK time = 0.00414s
-ZCOUNT time = 0.00445s
-ZRANGE time = 0.01570s
-ZREM time = 4.24112s
+$ make performance-server & make test-performance | grep 'zset Z'
+zset ZADD time = 0.45439s
+zset ZADD time = 0.67709s
+zset ZCARD time = 0.00124s
+zset ZRANK time = 0.00407s
+zset ZCOUNT time = 0.00450s
+zset ZRANGE time = 0.01875s
+zset ZREM time = 3.92562s
 
 
 $ redis-server --port 6376 & make test-performance | grep time
@@ -40,7 +40,7 @@ def test_zadd_same_score_all_elements():
     for score in range(LARGE_NUMBER):
         assert r.zadd('myzset', 0, 'value{}'.format(score)) == 1
     after_zadd = time.time()
-    print '\nZADD time = {:.5f}s'.format(after_zadd - before_zadd)
+    print '\nzset ZADD time = {:.5f}s'.format(after_zadd - before_zadd)
 
 
 def test_zadd_rescore_same_element():
@@ -49,7 +49,7 @@ def test_zadd_rescore_same_element():
     for score in range(LARGE_NUMBER):
         r.zadd('myzset', score, 'value')
     after_zadd = time.time()
-    print '\nZADD time = {:.5f}s'.format(after_zadd - before_zadd)
+    print '\nzset ZADD time = {:.5f}s'.format(after_zadd - before_zadd)
 
 
 def test_zcard():
@@ -59,7 +59,7 @@ def test_zcard():
     before_zcard = time.time()
     assert r.zcard('myzset') == LARGE_NUMBER
     after_zcard = time.time()
-    print '\nZCARD time = {:.5f}s'.format(after_zcard - before_zcard)
+    print '\nzset ZCARD time = {:.5f}s'.format(after_zcard - before_zcard)
 
 
 def test_zrank():
@@ -70,7 +70,7 @@ def test_zrank():
     assert r.zrank('myzset', 'value{}'.format(LARGE_NUMBER - 1)) == LARGE_NUMBER - 1
     after_zrank = time.time()
 
-    print '\nZRANK time = {:.5f}s'.format(after_zrank - before_zrank)
+    print '\nzset ZRANK time = {:.5f}s'.format(after_zrank - before_zrank)
 
 
 def test_zcount():
@@ -81,7 +81,7 @@ def test_zcount():
     assert r.zcount('myzset', '-inf', '+inf') == LARGE_NUMBER
     after_zcount = time.time()
 
-    print '\nZCOUNT time = {:.5f}s'.format(after_zcount - before_zcount)
+    print '\nzset ZCOUNT time = {:.5f}s'.format(after_zcount - before_zcount)
 
 
 def test_zrange():
@@ -92,7 +92,7 @@ def test_zrange():
     assert len(r.zrange('myzset', 0, LARGE_NUMBER)) == LARGE_NUMBER
     after_zrange = time.time()
 
-    print '\nZRANGE time = {:.5f}s'.format(after_zrange - before_zrange)
+    print '\nzset ZRANGE time = {:.5f}s'.format(after_zrange - before_zrange)
 
 
 def test_zrem():
@@ -107,4 +107,4 @@ def test_zrem():
         assert r.zrem('myzset', elem) == 1
     after_zrem = time.time()
 
-    print '\nZREM time = {:.5f}s'.format(after_zrem - before_zrem)
+    print '\nzset ZREM time = {:.5f}s'.format(after_zrem - before_zrem)
