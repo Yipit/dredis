8e918239b61c43e3d5f177251fc9aa2d792f7603
Hugo Lopes Tavares <hltbra@gmail.com>
2019-01-23 17:19:58 -0500

    Update zset performance results
    The current results are using ldb.batch_write()

--- tests-performance/test_zset_performance.py
+++ tests-performance/test_zset_performance.py
@@ -2,16 +2,16 @@
 The following results should serve as reference
 ------
 
-Results from 2019-01-18 on @htlbra's Macbook (LARGE_NUMBER == 1000):
+Results from 2019-01-23 on @htlbra's Macbook (LARGE_NUMBER == 1000):
 
 $ make performance-server & make test-performance | grep 'zset Z'
-zset ZADD time = 0.17577s
-zset ZADD time = 0.18646s
-zset ZCARD time = 0.00012s
-zset ZRANK time = 0.00338s
-zset ZCOUNT time = 0.00530s
-zset ZRANGE time = 0.00834s
-zset ZREM time = 0.16809s
+zset ZADD time = 0.16834s
+zset ZADD time = 0.16564s
+zset ZCARD time = 0.00011s
+zset ZRANK time = 0.00379s
+zset ZCOUNT time = 0.00536s
+zset ZRANGE time = 0.00872s
+zset ZREM time = 0.14646s
 
 
 $ redis-server --port 6376 & make test-performance | grep time
