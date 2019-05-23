81856db0715b0a74d87bc00182d248906b9c0b0d
Hugo Lopes Tavares <hltbra@gmail.com>
2019-05-23 10:49:03 -0400

    Experiment with bytearray to improve performance

--- dredis/parser.py
+++ dredis/parser.py
@@ -4,7 +4,7 @@ class Parser(object):
     CRLF = '\r\n'
 
     def __init__(self, read_fn):
-        self._buffer = ""
+        self._buffer = bytearray()
         self._buffer_pos = 0
         self._read_fn = read_fn
 
@@ -19,7 +19,7 @@ class Parser(object):
     def _read_into_buffer(self):
         # FIXME: implement a maximum size for the buffer to prevent a crash due to bad clients
         data = self._read_fn(self.MAX_BUFSIZE)
-        self._buffer += data
+        self._buffer.extend(data)
 
     def _read(self, n_bytes):
         if len(self._buffer[self._buffer_pos:]) < n_bytes:
@@ -27,7 +27,7 @@ class Parser(object):
         result = self._buffer[self._buffer_pos:][:n_bytes]
         # FIXME: ensure self.CRLF is next
         self._buffer_pos += n_bytes + len(self.CRLF)
-        return result
+        return str(result)
 
     def get_instructions(self):
         self._read_into_buffer()
