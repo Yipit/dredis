a3a07537e3ade5e1af1830c3b2aa8d229a26278b
Hugo Lopes Tavares <hltbra@gmail.com>
2019-06-05 12:55:20 -0400

    Use a single test instead of parametrized tests

--- tests/unit/test_dump_and_restore_fuzz.py
+++ tests/unit/test_dump_and_restore_fuzz.py
@@ -1,8 +1,6 @@
 import random
 import string
 
-import pytest
-
 
 def _get_random_value():
     return ''.join(random.choice(string.printable) for _ in range(int(random.random() * MAX_LENGTH)))
@@ -13,11 +11,13 @@ DATA_SIZE = 500
 FUZZY_DATA = [_get_random_value() for i in range(DATA_SIZE)]
 
 
-@pytest.mark.parametrize('fuzzy_value', FUZZY_DATA)
-def test_dump_and_restore_fuzzy_strings(keyspace, fuzzy_value):
+# NOTE: do not use pytest.mark.parametrize because it will create too many test cases
+# and make the output confusing.
+def test_dump_and_restore_fuzzy_strings(keyspace):
     key1 = 'test1'
     key2 = 'test2'
-    keyspace.set(key1, fuzzy_value)
-    keyspace.restore(key2, 0, keyspace.dump(key1), replace=False)
-    assert keyspace.get(key1) == fuzzy_value
-    assert keyspace.get(key2) == keyspace.get(key1)
+    for fuzzy_value in FUZZY_DATA:
+        keyspace.set(key1, fuzzy_value)
+        keyspace.restore(key2, 0, keyspace.dump(key1), replace=True)
+        assert keyspace.get(key1) == fuzzy_value
+        assert keyspace.get(key2) == keyspace.get(key1)
