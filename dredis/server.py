Yipit/dredis

commit 3b622cfb783c80f2d4f1136a8c566fd7d8179b7d

Change "debug" config dynamically

dredis/server.py
@@ -122,14 +122,6 @@ class RedisServer(asyncore.dispatcher):
             CommandHandler(sock)
 
 
-def setup_logging(level):
-    logger.setLevel(level)
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-
 def main():
     parser = argparse.ArgumentParser(version=__version__)
     parser.add_argument('--host', default='127.0.0.1', help='server host (defaults to %(default)s)')
@@ -159,9 +151,6 @@ def main():
 
     if args.debug:
         config.set('debug', 'true')
-        setup_logging(logging.DEBUG)
-    else:
-        setup_logging(logging.INFO)
 
     if args.readonly:
         config.set('readonly', 'true')

dredis/config.py
@@ -1,6 +1,8 @@
 import fnmatch
+import logging
 
 from dredis.exceptions import DredisError
+from dredis.utils import setup_logging
 
 TRUE = 'true'
 FALSE = 'false'
@@ -25,6 +27,14 @@ def get_all(pattern):
 
 def set(option, value):
     if option in _SERVER_CONFIG:
+        if option == 'debug':
+            value = _validate_bool(option, value)
+            if value == TRUE:
+                setup_logging(logging.DEBUG)
+            else:
+                setup_logging(logging.INFO)
+        elif option == 'readonly':
+            value = _validate_bool(option, value)
         _SERVER_CONFIG[option] = value
     else:
         raise DredisError('Unsupported CONFIG parameter: {}'.format(option))
@@ -32,3 +42,9 @@ def set(option, value):
 
 def get(option):
     return _SERVER_CONFIG[option]
+
+
+def _validate_bool(option, value):
+    if value.lower() not in (TRUE, FALSE):
+        raise DredisError("Invalid argument '{}' for CONFIG SET '{}'".format(value, option))
+    return value.lower()

dredis/utils.py
@@ -1,4 +1,6 @@
+import logging
 import struct
+import sys
 
 
 def to_float(s):
@@ -54,3 +56,12 @@ class FloatCodec(object):
 
 
 FLOAT_CODEC = FloatCodec()
+
+
+def setup_logging(level):
+    logger = logging.getLogger('dredis')
+    logger.setLevel(level)
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)

commit 9f54bd1690c40682ce54714d679894a73948edc2

Always set default configs
The logging setup only gets set up when we set the `debug` config.
This commit fixes the bug introduced by commit 3b622cfb783c80f2d4f1136a8c566fd7d8179b7d

dredis/server.py
@@ -149,14 +149,9 @@ def main():
     else:
         ROOT_DIR = tempfile.mkdtemp(prefix="redis-test-")
 
-    if args.debug:
-        config.set('debug', 'true')
-
-    if args.readonly:
-        config.set('readonly', 'true')
-
-    if args.requirepass:
-        config.set('requirepass', args.requirepass)
+    config.set('debug', config.TRUE if args.debug else config.FALSE)
+    config.set('readonly', config.TRUE if args.readonly else config.FALSE)
+    config.set('requirepass', args.requirepass if args.requirepass else config.EMPTY)
 
     db_backend_options = {}
     if args.backend_option: